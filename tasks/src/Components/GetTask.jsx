359b3e8927d5701d03f4279e9ea6c8eaed4662cc
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-04-08 10:44:26 +0530

    Updated

--- tasks/src/Components/GetTask.jsx
+++ tasks/src/Components/GetTask.jsx
@@ -160,26 +160,14 @@ const Task = ({ task, tasks, setTasks }) => {
       ref={drag}
       className={`relative p-4 mt-8 shadow-md rounded-md ${isDragging ? "opacity-25":"opacity-100"} cursor-grab`}
     >
-      <p>{task.name}</p>
+      <p>Name:-{" "}{task.name}</p>
+      <p>Status:-{" "}{task.status}</p>
       <button
-        className="absolute bottom-1 right-1 text-slate-400"
+        className="absolute bottom-1 right-1 text-red-400"
         onClick={() => handleRemove(task._id,task.id)
         }
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-          />
-        </svg>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRBXaR_x4cwDv5COtKyccXX1ZxwV8oYDHPYNLi2BAC_YcgroTYfZjs1aXfq2Pe-zS_D0E&usqp=CAU" width={30}/>
       </button>
     </div>
   );
